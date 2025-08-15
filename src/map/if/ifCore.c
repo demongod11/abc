551466f81e8567116f29857b564d9b5ebe502544
cchigarapally <cchigarapally@salesforce.com>
2025-08-15 23:10:48 +0530

    Displayed info about number of cuts exposed to mapper in if mapping

--- src/map/if/ifCore.c
+++ src/map/if/ifCore.c
@@ -194,6 +194,8 @@ int If_ManPerformMappingComb( If_Man_t * p )
 */
     p->pPars->FinalDelay = p->RequiredGlo;
     p->pPars->FinalArea  = p->AreaGlo;
+    // report total number of cuts considered during mapping when verbose is enabled
+    Abc_Print( 1, "Total K-feasible cuts considered = %d\n", p->nCutsTotal );
     return 1;
 }
 
